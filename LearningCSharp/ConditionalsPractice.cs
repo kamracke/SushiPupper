6ec38205414183b92f0c07734c9de0ee7c6b55b3
Kelsey McDonald <kamcdona@gmail.com>
2020-01-23 20:54:30 -0500

    Adding my practice code

---
+++ LearningCSharp/ConditionalsPractice.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Conditionals
+{
+    class ConditionalsPractice
+    {
+        public void Conditional1()
+        {
+            Console.WriteLine("How many dogs do you have?");
+            var input = Console.ReadLine();
+            var doggos = Convert.ToInt32(input);
+            if (doggos < 1)
+                Console.WriteLine("You need a doggo!");
+            else if (doggos >= 1 && doggos < 4)
+                Console.WriteLine("You are blessed to have doggos!");
+            else
+                Console.WriteLine("You may have too many doggos. :(");
+        }
+
+        public void Practice1()
+        {
+            Console.Write("Enter a number between 1 and 20: ");
+            var input = Console.ReadLine();
+            var number = Convert.ToInt32(input);
+            if (number >= 1 && number <= 20)
+                Console.WriteLine("Nice!");
+            else
+                Console.WriteLine("Not Cool. *sad face*");
+        }
+
+        public void Practice2()
+        {
+            Console.Write("Enter a number: ");
+            var number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter another number: ");
+            var number2 = Convert.ToInt32(Console.ReadLine());
+
+            var min = (number1 < number2) ? number1 : number2;
+            Console.WriteLine("Min is: " + min);
+        }
+
+        public void Practice3()
+        {
+            Console.Write("Enter the width: ");
+            var width = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the height: ");
+            var height = Convert.ToInt32(Console.ReadLine());
+
+            var orientation = width > height ? ImageOrientation.Landscape : ImageOrientation.Portrait;
+            Console.WriteLine("Image orientation is " + orientation);
+        }
+        public enum ImageOrientation
+        {
+            Landscape,
+            Portrait
+        }
+
+        public void Practice4()
+        {
+            Console.Write("Enter speed limit: ");
+            var speedlimit = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the speed of a car: ");
+            var speed = Convert.ToInt32(Console.ReadLine());
+
+            if (speed < speedlimit)
+                Console.WriteLine("Your speed is fine.");
+            else
+            {
+                const int kmPerDemeritPoint = 5;
+                var demeritPoints = (speed - speedlimit) / kmPerDemeritPoint;
+                if (demeritPoints > 12)
+                    Console.WriteLine("No more driving for you buddy! Gimme your license, stat.");
+                else
+                    Console.WriteLine("Demerit points: " + demeritPoints);
+            }
+        }
+        
+    }
+}
---
+++ LearningCSharp/LoopsPractice.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Loops
+{
+    public class LoopsPractice
+    {
+        public void Practice1()
+        {
+            var count = 0;
+            for (var i = 1; i <= 100; i++)
+            {
+                if (i%4 == 0)
+                    count++;
+            }
+            Console.WriteLine("There are {0} numbers divisible by 4 between 1 and 100.", count);
+        }
+
+        public void Practice2()
+        {
+            var sum = 0;
+            while (true)
+            {
+                Console.Write("Enter a number (or 'ok' to exit): ");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "ok")
+                    break;
+
+                sum += Convert.ToInt32(input);
+            }
+            Console.WriteLine("Sum of all numbers is: " + sum);
+        }
+
+        public void Practice3()
+        {
+            Console.Write("Enter a number: ");
+            var number = Convert.ToInt32(Console.ReadLine());
+
+            var factorial = 1;
+            for (var i = 1; i <= number; i++)
+                factorial *= i;
+
+            Console.WriteLine("{0}! = {1}", number, factorial);
+        }
+
+        public void Practice4()
+        {
+            var number = new Random().Next(1, 10);
+
+            Console.WriteLine("Secret is " + number);
+            for (var i = 0; i < 4; i++)
+            {
+                Console.Write("Guess the secret number: ");
+                var guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == number)
+                {
+                    Console.WriteLine("You won!");
+                    return;
+                }
+            }
+            Console.WriteLine("You lost!");
+        }
+
+        public void Practice5()
+        {
+            Console.Write("Enter comma separated numbers: ");
+            var input = Console.ReadLine();
+
+            var numbers = input.Split(',');
+
+            var min = Convert.ToInt32(numbers[0]);
+
+            foreach (var str in numbers)
+            {
+                var number = Convert.ToInt32(str);
+                if (number < min)
+                    min = number;
+            }
+            Console.WriteLine("Min is " + min);
+        }
+    }
+}
